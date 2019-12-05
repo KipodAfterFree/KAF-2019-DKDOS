8197a37b0984ebda5b821f5f904d766013ffb0fb
Omer Katz <omerk2511@gmail.com>
2019-12-05 23:40:58 +0200

    Initial commit.

---
+++ dkdos.asm
@@ -0,0 +1,151 @@
+IDEAL
+MODEL small
+STACK 100h
+DATASEG
+    message db 'MS-DOS KIPOD SHOP', 10, 13
+            db '------------------------------------------------', 10, 13, 10, 13
+            db 'Wanna buy some KIPODIM?', 10, 13
+            db 'First, I have to make sure you', 39, 're allowed to.', 10, 13, 10, 13
+            db 'Please log in with you password:', 10, 13, '$'
+
+    password db 9, 11 dup (0)
+    hash dw 0
+
+    validPass db 10, 13, 10, 13, 'Congrats! You are allowed to buy some KIPODIM!', 10, 13
+              db 'You can do it with `nc ctf.kaf.sh 5000`', 10, 13, '$'
+    invalidPass db 10, 13, 'No KIPODIM today :|', 10, 13, '$'
+
+CODESEG
+proc HashPassword
+    passwordLength equ [bp+8]
+    passwordAddress equ [bp+6]
+    hashAdress equ [bp+4]
+
+    push bp
+    mov bp, sp
+
+    mov bx, passwordAddress
+    mov cx, passwordLength
+
+HashLoop:
+    mov cl, [bx]
+    mov ah, 0
+
+    push bx
+        mov bx, hashAdress
+        shr [word ptr bx], 1
+        sub [word ptr bx], ax
+    pop bx
+
+    inc bx
+
+    dec cx
+    cmp cx, 0
+    jne HashLoop
+
+    pop bp
+
+    ret
+endp HashPassword
+
+proc DisplayMessage
+    lea dx, [message]
+    mov ah, 9h
+    int 21h
+    
+    ret
+endp DisplayMessage
+
+proc GetPassword
+    mov [word ptr cs:13h], 0701h
+
+    lea dx, [password]
+    mov ah, 0Ah
+    int 21h
+    
+    ret
+endp GetPassword
+
+proc Branch
+    pop ax
+
+    lea bx, [cs:071h]
+    cmp ax, bx
+
+    je InvalidPassword
+
+    lea bx, [cs:097h]
+    cmp ax, bx
+
+    je ValidPassword
+
+    lea bx, [cs:0A1h]
+    cmp ax, bx
+
+    je Exit
+
+    jmp ax
+endp Branch
+
+Start:
+	mov ax, @data
+	mov ds, ax
+
+    call DisplayMessage
+    
+    mov [word ptr cs:11h], 027D1h
+
+    call GetPassword
+
+    mov al, [password+9]
+    test al, al
+    jz JumpToInvalidPassword
+
+    cmp [password+1], 8
+    je CallHashPassword
+
+JumpToInvalidPassword:
+    call Branch ; Here -> invalid
+
+CallHashPassword:
+    lea bx, [password+1]
+
+    mov cl, [bx]
+    mov ch, 0
+
+    push cx
+
+    inc bx
+    push bx
+
+    lea bx, [hash]
+    push bx
+
+    mov [word ptr cs:09h], 078Ah
+
+    call HashPassword
+    add sp, 6
+
+VerifyPassword:
+    cmp [word ptr hash], 43E5h
+    jne InvalidPassword
+
+    call Branch ; Here -> valid
+
+InvalidPassword:
+    lea dx, [invalidPass]
+    mov ah, 9h
+    int 21h
+
+    call Branch ; Here -> exit
+
+ValidPassword:
+    lea dx, [validPass]
+    mov ah, 9h
+    int 21h
+
+Exit:
+	mov ax, 4c00h
+	int 21h
+
+END Start
---
+++ server.py
@@ -0,0 +1,60 @@
+from threading import Lock, Thread
+from time import sleep
+from socket import SO_REUSEADDR, SOCK_STREAM, socket, SOL_SOCKET, AF_INET
+
+CLIENT_MESSAGE = """MS-DOS KIPOD SHOP
+------------------------------------------------
+Wanna buy some KIPODIM?
+First, I have to make sure you're allowed to.
+Please log in with you password: """
+
+FLAG = 'kaf{D05_15_JU57_700_C00L}'
+INVALID = 'Man this is not the right password :('
+
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+s.bind(('localhost', 8000))
+
+threads = []
+
+def hash(password):
+    sleep(1)
+
+    if len(password) != 8:
+        return 0
+
+    calculated_hash = 0
+
+    for char in password:
+        calculated_hash = ((calculated_hash << 1) + ord(char)) & 0xffff
+
+    return calculated_hash
+
+class ClientHandler(Thread):
+    def __init__(self, address, port, socket):
+        Thread.__init__(self)
+        self.address = address
+        self.port = port
+        self.socket = socket
+
+    def run(self):
+        self.socket.send(CLIENT_MESSAGE)
+
+        password = self.socket.recv(4096).replace('\n', '')
+
+        if hash(password) == 0x43e5:
+            self.socket.send('\n' + FLAG)
+        else:
+            self.socket.send('\n' + INVALID)
+
+        self.socket.close()
+
+while 1:
+    s.listen(1)
+    
+    sock, addr = s.accept()
+    
+    newThread = ClientHandler(addr[0], addr[1], sock)
+    newThread.start()
+    
+    threads.append(newThread)
